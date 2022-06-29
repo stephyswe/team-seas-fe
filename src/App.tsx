26655a96e17610b97c77fbf8811ab0f3afd2db70
Stephanie <stephaniiehallberg@gmail.com>
2022-06-29 22:44:42 +0200

    feat(app): counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,77 @@
-import * as React from "react"
+import '@fontsource/montserrat/700.css'
+import '@fontsource/montserrat/400.css'
+import '@fontsource/montserrat/300.css'
+
 import {
   ChakraProvider,
   Box,
   Text,
-  Link,
   VStack,
-  Code,
   Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+  Heading,
+  extendTheme
+} from '@chakra-ui/react'
+import { ColorModeSwitcher } from './ColorModeSwitcher'
+import { Logo } from './Logo'
+import { Counter } from './donation/Counter'
+
+const TotalDonationsQuery = `
+  query Query {
+    totalDonations
+  }
+`;
+
+const TotalUpdatedQuery = `
+  subscription Subscription {
+    totalUpdated {
+      total
+    }
+  }
+`;
+
+const handleSubscription = (previous: any, newTotal: any) => {
+  return newTotal?.totalUpdated?.total;
+};
+
+const theme = extendTheme({
+  fonts: {
+    heading: 'Montserrat',
+    body: 'Montserrat'
+  }
+})
 
 export const App = () => (
+  /* const [res] = useSubscription(
+    { query: TotalUpdatedQuery },
+    handleSubscription
+  );
+  const [{ data, fetching, error }] = useQuery({
+    query: TotalDonationsQuery,
+  });
+
+  if (fetching) return <p>Loading...</p>;
+  if (error) return <p>Oh no... {error.message}</p>; */
+
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
+      <Grid minH="100vh" p={3} bg="gray.500">
         <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
+          <Logo h="32" pointerEvents="none" />
+          <Heading as="h1" size="xl">
+            JOIN THE MOVEMENT!
+          </Heading>
+
           <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+            The team is growing everyday and scoring wins for the planet.
+            <br /> Remove trash with us and track our progress!
           </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
+          <Heading as="h2" size="4xl">
+            <Counter from={0} to={3244443} />
+          </Heading>
+          {/* <DonationWizard />
+
+            <Leaderboard /> */}
         </VStack>
       </Grid>
     </Box>
--- src/Logo.tsx
+++ src/Logo.tsx
@@ -1,24 +1,8 @@
-import * as React from "react"
 import {
-  chakra,
-  keyframes,
-  ImageProps,
-  forwardRef,
-  usePrefersReducedMotion,
-} from "@chakra-ui/react"
-import logo from "./logo.svg"
+  chakra, forwardRef, ImageProps
+} from '@chakra-ui/react';
+import logo from './TeamSeasLogo.png';
 
-const spin = keyframes`
-  from { transform: rotate(0deg); }
-  to { transform: rotate(360deg); }
-`
-
-export const Logo = forwardRef<ImageProps, "img">((props, ref) => {
-  const prefersReducedMotion = usePrefersReducedMotion()
-
-  const animation = prefersReducedMotion
-    ? undefined
-    : `${spin} infinite 20s linear`
-
-  return <chakra.img animation={animation} src={logo} ref={ref} {...props} />
-})
+export const Logo = forwardRef<ImageProps, 'img'>((props, ref) => {
+  return <chakra.img src={logo} ref={ref} {...props} />;
+});
