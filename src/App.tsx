67542898860a4f191f91811a902b1ef3450b1279
Stephanie <stephaniiehallberg@gmail.com>
2022-06-29 23:39:22 +0200

    feat(app): urql subscription

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import '@fontsource/montserrat/700.css'
 import '@fontsource/montserrat/400.css'
 import '@fontsource/montserrat/300.css'
 
+import { useQuery, useSubscription } from 'urql'
 import {
   ChakraProvider,
   Box,
@@ -19,7 +20,7 @@ const TotalDonationsQuery = `
   query Query {
     totalDonations
   }
-`;
+`
 
 const TotalUpdatedQuery = `
   subscription Subscription {
@@ -27,11 +28,11 @@ const TotalUpdatedQuery = `
       total
     }
   }
-`;
+`
 
 const handleSubscription = (previous: any, newTotal: any) => {
-  return newTotal?.totalUpdated?.total;
-};
+  return newTotal?.totalUpdated?.total
+}
 
 const theme = extendTheme({
   fonts: {
@@ -40,40 +41,42 @@ const theme = extendTheme({
   }
 })
 
-export const App = () => (
-  /* const [res] = useSubscription(
+export const App = () => {
+  const [res] = useSubscription(
     { query: TotalUpdatedQuery },
     handleSubscription
-  );
+  )
   const [{ data, fetching, error }] = useQuery({
-    query: TotalDonationsQuery,
-  });
+    query: TotalDonationsQuery
+  })
 
-  if (fetching) return <p>Loading...</p>;
-  if (error) return <p>Oh no... {error.message}</p>; */
+  if (fetching) return <p>Loading...</p>
+  if (error) return <p>Oh no... {error.message}</p>
 
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3} bg="gray.500">
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="32" pointerEvents="none" />
-          <Heading as="h1" size="xl">
-            JOIN THE MOVEMENT!
-          </Heading>
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3} bg="gray.500">
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <VStack spacing={8}>
+            <Logo h="32" pointerEvents="none" />
+            <Heading as="h1" size="xl">
+              JOIN THE MOVEMENT!
+            </Heading>
 
-          <Text>
-            The team is growing everyday and scoring wins for the planet.
-            <br /> Remove trash with us and track our progress!
-          </Text>
-          <Heading as="h2" size="4xl">
-            <Counter from={0} to={3244443} />
-          </Heading>
-          {/* <DonationWizard />
+            <Text>
+              The team is growing everyday and scoring wins for the planet.
+              <br /> Remove trash with us and track our progress!
+            </Text>
+            <Heading as="h2" size="4xl">
+              <Counter from={data.totalDonations} to={res.data || data.totalDonations} />
+            </Heading>
+            {/* <DonationWizard />
 
             <Leaderboard /> */}
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+          </VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  )
+}
